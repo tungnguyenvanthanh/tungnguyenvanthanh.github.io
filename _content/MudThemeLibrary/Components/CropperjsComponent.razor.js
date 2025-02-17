8b38e64f09a44b47b4ca75868c8dcf3abf5b169a
tungnguyenvanthanh@hotmail.com <Nguyn Vn Thanh Tng>
2025-02-17 13:16:36 +0700

    up

---
+++ _content/MudThemeLibrary/Components/CropperjsComponent.razor.js
@@ -0,0 +1,75 @@
+﻿export let Cropperjs = function () {
+    let dynamicCss = (href) => {
+        return new Promise((resolve, reject) => {
+            if (document.querySelector(`link[href="${href}"]`)) {
+                resolve(); // Nếu CSS đã được load thì không load lại
+                return;
+            }
+
+            let link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = href;
+            link.onload = () => resolve();
+            link.onerror = () => reject(`Failed to load CSS: ${href}`);
+
+            document.head.appendChild(link);
+        });
+    }
+
+    let init = (imageRef) => {
+        let cropper = new Cropper(imageRef, {
+            aspectRatio: 1,
+            viewMode: 0,
+        });
+
+        return cropper;
+    }
+
+    let croppedCanvas = (cropper) => {
+        let result = cropper["getCroppedCanvas"]();
+        if (result instanceof HTMLCanvasElement) {
+            return result.toDataURL("image/jpeg");
+        }
+    }
+
+    let rotate = (cropper, option) => {
+        cropper["rotate"](option, undefined);
+        return cropper;
+    }
+
+    let scaleX = (cropper, option) => {
+        cropper["scaleX"](option, undefined);
+        return cropper;
+    }
+
+    let scaleY = (cropper, option) => {
+        cropper["scaleY"](option, undefined);
+        return cropper;
+    }
+
+    return {
+        dynamicCss: (href) => {
+            dynamicCss(href);
+        },
+
+        init: (imageRef) => {
+            return init(imageRef);
+        },
+
+        croppedCanvas: (cropper) => {
+            return croppedCanvas(cropper);
+        },
+
+        rotate: (cropper, option) => {
+            return rotate(cropper, option);
+        },
+
+        scaleX: (cropper, option) => {
+            return scaleX(cropper, option);
+        },
+
+        scaleY: (cropper, option) => {
+            return scaleY(cropper, option);
+        }
+    };
+}();
---
+++ _content/MudThemeLibrary/js/scripts.bundle.js
@@ -0,0 +1,83 @@
+﻿function loadScript(url, location) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = false; // Đảm bảo tải theo thứ tự
+        script.onload = () => resolve(url);
+        script.onerror = () => reject(new Error(`Failed to load ${url}`));
+
+        switch (location) {
+            case "head":
+                document.head.appendChild(script);
+                break;
+            case "body":
+                document.body.appendChild(script);
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+function loadCSS(url, location) {
+    return new Promise((resolve, reject) => {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = url;
+        link.onload = () => resolve(url);
+        link.onerror = () => reject(new Error(`Failed to load ${url}`));
+
+        switch (location) {
+            case "before":
+                document.head.insertBefore(link, document.head.firstChild);
+                break;
+            case "after":
+                document.head.appendChild(link);
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+// Danh sách file JS cần tải
+const scriptsToLoadHead = [
+    "_content/MudThemeLibrary/js/jquery/jquery-3.7.1.js",
+    "_content/MudThemeLibrary/js/jquery/jquery-ui.js",
+];
+
+const scriptsToLoadBody = [
+    "_content/MudBlazor/MudBlazor.min.js",
+    "_content/CurrieTechnologies.Razor.SweetAlert2/sweetAlert2.min.js",
+    "_content/MudThemeLibrary/plugins/lottie/lottie.min.js",
+    "_content/MudThemeLibrary/plugins/cropperjs/cropper.js",
+];
+
+const stylesToLoad = [
+    "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap",
+    "https://use.fontawesome.com/releases/v5.14.0/css/all.css",
+    "_content/MudBlazor/MudBlazor.min.css",
+    "_content/MudThemeLibrary/plugins/cropperjs/cropper.css",
+    "_content/MudThemeLibrary/css/app.css",
+    "_content/MudThemeLibrary/css/spacing.css",
+];
+
+// Tải các file CSS theo thứ tự
+(async function () {
+    for (let style of stylesToLoad.reverse()) { // reverse() đảo ngược mảng stylesToLoad
+        await loadCSS(style, "before");
+        console.log(`${style} loaded`);
+    }
+})();
+
+// Tải các file theo thứ tự
+(async function () {
+    for (let script of scriptsToLoadHead) {
+        await loadScript(script, "head");
+        console.log(`${script} loaded`);
+    }
+    for (let script of scriptsToLoadBody) {
+        await loadScript(script, "body");
+        console.log(`${script} loaded`);
+    }
+})();
