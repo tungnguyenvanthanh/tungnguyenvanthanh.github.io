5ebaf23c1a14a986007cc8e8f16a8c6cb0aa792c
tungnguyenvanthanh@hotmail.com <Nguyn Vn Thanh Tng>
2025-02-19 11:54:20 +0700

    up

--- _content/MudThemeLibrary/Theme/LottieSVGTheme.razor.js
+++ _content/MudThemeLibrary/Theme/LottieSVGTheme.razor.js
@@ -1,22 +1,24 @@
 ﻿export let LottieSVG = function () {
-    let init = (el, jsonUrl, loop) => {
+    let init = (el, jsonUrl, loop, speed) => {
         if (el === null) {
             return;
         }
 
         el.innerHTML = "";
-        lottie.loadAnimation({
+        var animate = lottie.loadAnimation({
             container: el,
             renderer: 'svg',
             loop: loop,
             autoplay: true,
             path: jsonUrl
         });
+
+        animate.setSpeed(speed);
     }
 
     return {
-        init: (el, jsonUrl, loop) => {
-            init(el, jsonUrl, loop);
+        init: (el, jsonUrl, loop, speed) => {
+            init(el, jsonUrl, loop, speed);
         }
     };
 }();
