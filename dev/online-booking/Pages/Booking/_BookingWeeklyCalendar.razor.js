c514e0d18bdf11fa222608f9319af31dbac1b9c9
Nguyễn Văn Thanh Tùng <tungnguyenvanthanh@hotmail.com>
2025-03-02 05:49:18 +0700

    up

---
+++ dev/online-booking/Pages/Booking/_BookingWeeklyCalendar.razor.js
@@ -0,0 +1,35 @@
+﻿export let BookingWeeklyCalendar = function () {
+    let render = (weekContainer, weekHeader, weekData, dotNetHelper) => {
+        weekContainer.innerHTML = "";
+        weekHeader.innerHTML = "";
+
+        weekData.forEach((day, index) => {
+            let weekdayDiv = document.createElement("div");
+            weekdayDiv.classList.add("day");
+            weekdayDiv.innerHTML = `<span class="weekday">${day.weekday}</span>`;
+            weekHeader.appendChild(weekdayDiv);
+
+            let dayDiv = document.createElement("div");
+            dayDiv.classList.add("day");
+            if (day.isToday) {
+                dayDiv.classList.add("today");
+            }
+            if (day.isSelected) {
+                dayDiv.classList.add("selected");
+            }
+
+            const date = new Date(day.dayText);
+            const formattedDate = `${date.getFullYear() }-${date.getMonth() + 1}-${date.getDate() }`;
+
+            dayDiv.innerHTML = `<span class="weekday">${date.getDate()}</span>`;
+            dayDiv.onclick = () => dotNetHelper.invokeMethodAsync('OnSelectedDate', formattedDate);
+            weekContainer.appendChild(dayDiv);
+        });
+    }
+
+    return {
+        render: (weekContainer, weekHeader, weekData, dotNetHelper) => {
+            render(weekContainer, weekHeader, weekData, dotNetHelper);
+        }
+    };
+}();
