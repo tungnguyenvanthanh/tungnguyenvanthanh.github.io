08fcc176e705fa99a48730d6d4c9557098393500
Nguyễn Văn Thanh Tùng <tungnguyenvanthanh@hotmail.com>
2025-03-02 07:06:10 +0700

    up

--- dev/online-booking/Pages/Booking/_BookingWeeklyCalendar.razor.js
+++ dev/online-booking/Pages/Booking/_BookingWeeklyCalendar.razor.js
@@ -19,10 +19,22 @@
             }
 
             const date = new Date(day.dayText);
+
+            let d1 = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+            let d2 = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+
+            if (d1 < d2) {
+                dayDiv.classList.add("disabled");
+            }
+
             const formattedDate = `${date.getFullYear() }-${date.getMonth() + 1}-${date.getDate() }`;
 
             dayDiv.innerHTML = `<span class="weekday">${date.getDate()}</span>`;
-            dayDiv.onclick = () => dotNetHelper.invokeMethodAsync('OnSelectedDate', formattedDate);
+
+            if (d1 >= d2) {
+                dayDiv.onclick = () => dotNetHelper.invokeMethodAsync('OnSelectedDate', formattedDate);
+            }
+
             weekContainer.appendChild(dayDiv);
         });
     }
