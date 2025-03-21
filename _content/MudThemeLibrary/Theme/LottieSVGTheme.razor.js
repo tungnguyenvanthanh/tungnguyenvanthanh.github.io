3551ab41e225a6613117a5cf10c18a8e0684c5fb
tungnguyenvanthanh@hotmail.com <Nguyn Vn Thanh Tng>
2025-03-21 08:39:40 +0700

    up

--- _content/MudThemeLibrary/Theme/LottieSVGTheme.razor.js
+++ _content/MudThemeLibrary/Theme/LottieSVGTheme.razor.js
@@ -5,15 +5,22 @@
         }
 
         el.innerHTML = "";
-        var animate = lottie.loadAnimation({
-            container: el,
-            renderer: 'svg',
-            loop: loop,
-            autoplay: true,
-            path: jsonUrl
-        });
 
-        animate.setSpeed(speed);
+        try {
+            var animate = lottie.loadAnimation({
+                container: el,
+                renderer: 'svg',
+                loop: loop,
+                autoplay: true,
+                path: jsonUrl
+            });
+
+            animate.setSpeed(speed);
+        } catch (e) {
+            setTimeout(() => {
+                init(el, jsonUrl, loop, speed);
+            }, 2000)
+        }
     }
 
     return {
